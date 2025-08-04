0189afbefad76622422b29eceaa443f690b615e9
SREENATHREDDY1234 <153479961+SREENATHREDDY1234@users.noreply.github.com>
2025-08-04 22:28:40 +0530

    2sum problem

---
+++ Array/twoSum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> map;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (map.count(complement)) {
+            return {map[complement], i};
+        }
+        map[nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    for (int i : result) {
+        cout << i << " ";
+    }
+    return 0;
+}
